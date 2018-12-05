94195d7ebb4e27e8098923520dc2452b0c36a607
drubaby <peryton6@gmail.com>
2018-12-05 18:39:56 -0500

    add IPDB link to LocMac card

--- src/components/LocationMachineCard.js
+++ src/components/LocationMachineCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card } from "semantic-ui-react";
 import MachineModal from "./MachineModal";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { selectLocationMachine } from "../redux/actions/locationActions";
 
@@ -9,12 +10,13 @@ class LocationMachineCard extends Component {
 // renders MachineModal
   render() {
     let { machine, machine_issues} = this.props.locMacObj;
-
+    let link_text = "https://www.ipdb.org/machine.cgi?id=" + `${machine.ipdb_id}`
     return (
       <Card fluid>
         <Card.Content>
           <Card.Header>{machine.name}</Card.Header>
           <Card.Meta>({machine.manufacture_date})</Card.Meta>
+          <Card.Description><a href={link_text} target='_blank'>IPDB Entry</a></Card.Description>
           {this.props.is_working ? "Working" : "Out of Order"}
           <Card.Description>
             {machine_issues.length} known issues
